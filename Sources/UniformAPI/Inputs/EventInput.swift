bfd013f05ffd68fe73b889e80bdfc0be3ffe996a
Jordan Kay <jordan@fleuronic.com>
2025-10-27 20:11:08 -0400

    Update event input

--- Sources/UniformAPI/Inputs/EventInput.swift
+++ Sources/UniformAPI/Inputs/EventInput.swift
@@ -9,6 +9,8 @@ import protocol Caesura.Input
 
 struct EventInput {
 	let date: Date
+	let detailsURL: URL?
+	let scoresURL: URL?
 	let locationID: Location.ID
 	let circuitID: Circuit.ID?
 	let showID: Show.ID?
@@ -24,6 +26,8 @@ extension EventInput: Input {
 			\.location == locationID
 		]
 
+		detailsURL.map { valueSet = valueSet.update(with: [\.value.detailsURL == $0]) }
+		scoresURL.map { valueSet = valueSet.update(with: [\.value.scoresURL == $0]) }
 		circuitID.map { valueSet = valueSet.update(with: [\.circuit == $0]) }
 		showID.map { valueSet = valueSet.update(with: [\.show == $0]) }
 		venueID.map { valueSet = valueSet.update(with: [\.venue == $0]) }
