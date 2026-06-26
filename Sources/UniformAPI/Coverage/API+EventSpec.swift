500b360b5b50aadaac74fc057bb05b13a9ebce68
Jordan Kay <jordan@fleuronic.com>
2026-06-26 17:23:09 -0400

    Scope event modifications to the season's publish-date window

--- Sources/UniformAPI/Coverage/API+EventSpec.swift
+++ Sources/UniformAPI/Coverage/API+EventSpec.swift
@@ -77,7 +77,7 @@ private extension API {
 	func eventModifications(for year: Int) async throws -> [URL: String] {
 		guard year >= 2024 else { return [:] }
 
-		let apiURL = URL(string: "https://www.dci.org/wp-json/wp/v2/event?per_page=100&orderby=modified&order=desc")!
+		let apiURL = URL(string: "https://www.dci.org/wp-json/wp/v2/event?per_page=100&after=\(year - 1)-09-01T00:00:00&before=\(year)-09-01T00:00:00")!
 		let (data, _) = try await scraperSession.data(from: apiURL)
 		let events = try! JSONSerialization.jsonObject(with: data) as! [[String: Any]]
 
